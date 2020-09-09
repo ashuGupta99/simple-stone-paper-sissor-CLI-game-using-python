b4bcbee0e721573ee65f7507b2dd162f4bbc25fe
prince <guptaash974@gmail.com>
2020-09-09 12:58:16 +0530

    Reduse number of lines

--- addition of twono.cxx
+++ addition of twono.cxx
@@ -1,36 +1,22 @@
-#include<iostream.h>
-#include<conio.h>
-
-int a,b,s;
-int calc(int);
-int calc(int,int);
-void main()
-{
-	clrscr();
+#include<iostream>
+int calc(int x);
+int calc(int x,int y);
+int main(){
+  //function overloading
+  int a,b,s;
+	std::cout<<"enter a number"<<std::endl;
+	std::cin>>s;
+	std::cout<<"square"<<"("<<s<<"x"<<s<<")"<<"="<<calc(s)<<std::endl;
 	
-	cout<<"enter a number"<<endl;
-	cin>>s;
-	cout<<"square"<<"("<<s<<"x"<<s<<")"<<"="<<calc(s)<<endl;
-//	cout<<s<<"x"<<s<<"="<<calc(s)<<endl;
-	
-	cout<<"enter any two number " <<endl;
-	cin>>a>>b;
-cout<<"addition"<<"("<<a<<"+"<<b<<")"<<"="<<calc(a,b);
-//	cout<<a<<"+"<<b<<"="<<calc(a,b);
-	getch();
+	std::cout<<"enter any two number " <<std::endl;
+	std::cin>>a>>b;
+  std::cout<<"addition"<<"("<<a<<"+"<<b<<")"<<"="<<calc(a,b)<<std::endl;
+	return 0;
 }
-	
-
- 
- int calc(int x)
- {
- 	x=s;
+ int calc(int x) {
  	return (x * x);
  }
  
- int calc(int x,int y)
- {
- 	x=a;
- 	y=b;
+ int calc(int x,int y) {
  	return (x + y);
  }
